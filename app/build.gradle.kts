ef8f36abcfb9907cf70aaedcdfa722735ce39e84
Giovanni <cuzztomgdev@gmail.com>
2025-03-15 22:39:02 -0600

    Libraries installed to dependency injection, make requests and load images

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,11 @@
+val PrivateApiKey = project.findProperty("PrivateKey")
+val PublicApiKey = project.findProperty("PublicKey")
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.ksp)
+    alias(libs.plugins.android.hilt)
 }
 
 android {
@@ -14,7 +19,15 @@ android {
         versionCode = 1
         versionName = "1.0"
 
+        buildConfigField("String", "PRIVATE_API_KEY", "\"${PrivateApiKey}\"")
+        buildConfigField("String", "PUBLIC_API_KEY", "\"${PublicApiKey}\"")
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
     }
 
     buildTypes {
@@ -26,6 +39,7 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -42,6 +56,25 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    //Nav
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    //Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.dagger.compiler)
+    ksp(libs.hilt.compiler)
+
+    //Retrofit
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.converter.gson)
+    implementation(libs.okhttp.interceptor)
+
+    //Glide
+    implementation(libs.glide)
+    ksp(libs.glide.compiler)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
